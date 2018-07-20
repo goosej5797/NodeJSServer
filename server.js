9ce9423fcb5cdc4bed66b46501abd93a357e6f3a
Joshua Goosen <goose1jd@cmich.edu>
2018-07-20 10:33:21 -0400

    Added functionality for an api call to have parameters passed to it

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var path = require("path");
 var fs = require("fs");
+var url = require('url');
 
 fs.readFile("config/info.json", function(err, data){
     if(err){
@@ -16,23 +17,22 @@ fs.readFile("config/info.json", function(err, data){
 function setUpServer(serverInfo){
     console.log("Starting web server at " + serverInfo["serverUrl"] + ":" + serverInfo["port"]);
     http.createServer( function(req, res) {
-        var filename = req.url;
-        var extension = path.extname(filename);
-        if(extension){
-            serveFileCall(filename, res);
+        var parsedUrl = url.parse(req.url, true);
+        if(path.extname(parsedUrl.href)){
+            serveFileCall(parsedUrl, res);
         }else{
-            serveApiCall(filename, res);
+            serveApiCall(parsedUrl, res);
         }
     }).listen(serverInfo["port"], serverInfo["serverUrl"]);
 }
 
-function serveApiCall(filename, res){
+function serveApiCall(parsedUrl, res){
     //Initial check for first part of string
     //to make sure it is an actual API call
-    if(filename.substring(0,4) == '/api'){
+    if(parsedUrl.pathname.substring(0,4) == '/api'){
         //Call specific API...If API doesn't exist
         //a 500 is returned to the sender
-        if(apiCall(filename.substring(4))){
+        if(apiCall(parsedUrl)){
             res.writeHead(200);
         }else{
             res.writeHead(500);
@@ -44,8 +44,8 @@ function serveApiCall(filename, res){
     res.end();
 }
 
-function serveFileCall(filename, res){
-    var localPath = __dirname + filename;
+function serveFileCall(parsedUrl, res){
+    var localPath = __dirname + parsedUrl.pathname;
     fs.exists(localPath, function(exists) {
         if(exists) {
             console.log("Serving file: " + localPath);
@@ -70,8 +70,8 @@ function getFile(localPath, res) {
 	});
 }
 
-function apiCall(apiPath){
-    switch(apiPath){                                      
+function apiCall(parsedUrl){
+    switch(parsedUrl.pathname.substring(4)){                                      
         case "/":   //Example
             //Call API function here --- ex."testApiCall();"
             break;
